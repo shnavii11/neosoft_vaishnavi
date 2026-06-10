bf9309347617ddcca678b33ddc3ecd589ce6f497
Vaishnavi Sanap <vdsanap_b24@el.vjti.ac.in>
2026-06-10 16:39:48 +0530

    Enhance comments in twoSum method
    Added comments to clarify the twoSum algorithm's logic.

--- tas1.py
+++ tas1.py
@@ -1,14 +1,17 @@
 #2sum should match the given target 
+#brutefore = would be to verify every number with the next and other nuber to optimise this we rater see the need of number remaining
 class Solution:
       def twoSum(self, nums, target):
+      #store already read numbers 
           read = {}
 
           for i, a in enumerate(nums):
+          # number required to reach the target
               remaining = target - a
-
+         # if that x nuber was seen before return 
               if remaining in read:
                   return [read[remaining], i]
 
               read[a] = i
-
+         # else return null 
           return [] 
